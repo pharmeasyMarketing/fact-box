4e9fc7beae0d641d3c9b2b6ddd53dc865e45f68d
pharmeasyMarketing <134919459+pharmeasyMarketing@users.noreply.github.com>
2023-07-10 15:30:28 +0530

    Update script.py

--- script.py
+++ script.py
@@ -166,8 +166,6 @@ def pubmed_article_scrap(url):
 #     response = response
 #     return response
 
-
-
 def generate_facts(list_of_article, topic, num_facts, model="gpt-3.5-turbo-16k", max_tokens=3000, temperature=0.2):
     if len(list_of_article) > 15500:
         # Split the list_of_article into three parts
