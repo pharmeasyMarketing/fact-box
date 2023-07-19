0de0277db72dbc09b8b6678aa13e52de24ea0d50
pharmeasyMarketing <134919459+pharmeasyMarketing@users.noreply.github.com>
2023-07-19 12:07:27 +0530

    Update script.py
    token problem removed

--- script.py
+++ script.py
@@ -5,8 +5,13 @@ from bs4 import BeautifulSoup, Tag
 import pandas as pd
 import urllib.parse
 from newspaper import Article
+from bs4 import BeautifulSoup
+import re
 import openai
 import random
+
+import nltk
+nltk.download('punkt')
 
 pd.set_option("display.max_colwidth", None)
 
@@ -93,14 +98,7 @@ def filtered_url(df):
     "https://www.statista.com/"
     ]
 
-    # flag = 0
-    # for index, row in df.iterrows():
-    #     url = row["URL"]
-    #     for website in website_list:
-    #         if website in url:
-    #             df.loc[flag, "Filtered Url"] = url
-    #             flag += 1       
-    # return df
+
     filtered_urls = set()
     for index, row in df.iterrows():
         url = row["URL"]
@@ -119,21 +117,7 @@ def scrape_article(url):
         article.parse()
         return article.text
     except:
-        return "Access denied"
-
-# def get_article(url):
-#     response = requests.get(url)
-#     if response.status_code == 403:
-#         # We've been forbidden, so try a few different things
-#         for i in range(10):
-#             user_agent = random.choice(USER_AGENTS)
-#             response = requests.get(url, headers={'User-Agent': user_agent})
-#             if response.status_code == 200:
-#                 return response.content
-#     elif response.status_code != 200:
-#         # Something else went wrong
-#         raise Exception('Error getting article: {}'.format(response.status_code))
-#     return response.content
+        return ""
 
 def get_article(url):
     headers = {'User-Agent': random.choice(USER_AGENTS)}
@@ -157,74 +141,180 @@ def pubmed_article_scrap(url):
         content = get_article(url)
         soup = BeautifulSoup(content, 'html.parser')
         text = [p.get_text() for p in soup.find_all('p')]
-        # text = '\n'.join(text)
+        text = '\n'.join(text)
         return text
     except:
-        return " Access denied"
+        return ""
 
 
-# def generate_facts(list_of_article, topic, model="gpt-3.5-turbo-32k", max_tokens=1000, temperature=0.2):
 
-#     prompt = f'''
-#     You are a specialized medical researcher or practitioner. A collection of articles titled '{list_of_article}' that delve into the subject matter of '{topic}' has been handed to you. Each article comes with the web page source from which the data has been procured. Your role involves conducting a detailed review and comprehension of these articles, discarding any superfluous information, and subsequently drawing out 10 significant data-driven facts pertaining to the provided topic. Each fact needs to be brief, straightforward, and formatted as a one-liner. These facts are to be organized in a numbered list with the accompanying source link for each fact, denoting the domain name of the source website.
 
-#     Guidelines:
-#     - Ensure the facts are grounded in data, incorporating quantifiable numbers or percentages as much as possible.
-#     - shorten and keep the points crisp and strictly under 10 words.
-#     - If fact is about any research consducted which is clinically not proved. Never add that fact.
-#     - The facts should be arranged in an ordered, numbered list.
-#     - Each fact should include the source in the format '[source: [domain_name](URL)]'. Replace 'domain_name' with the actual domain name from the list of articles, and 'URL' with the complete URL from the article list. The source should be hyperlinked with the anchor text representing the source name.
-#     - You are strictly prohibited from creating an extra sources section. Instead, hyperlink the URL in the anchor text at the end of each fact.
+def count_tokens( list_of_article, topic, num_facts):
 
-#     ''' 
+    prompt = f'''
+    You are a specialized medical researcher or practitioner. A collection of articles titled '{list_of_article}' that delve into the subject matter of '{topic}' has been handed to you. Each article comes with the web page source from which the data has been procured. Your role involves conducting a detailed review and comprehension of these articles, discarding any superfluous information, and subsequently drawing out {num_facts} crisp facts to the provided topic. Each fact needs to be straightforward, and formatted under 15 words. These facts are to be organized in a numbered list with the accompanying source link for each fact, denoting the domain name of the source website.
 
-#     gpt_response = openai.ChatCompletion.create(
-#         model=model,
-#         messages=[
-#             {
-#                 "role": "system",
-#                 "content":  prompt,
-#             },
-#             {"role": "user", "content": f"topic: {topic}, list of articles: {list_of_article} "},
-#         ],
-#         max_tokens=max_tokens,
-#         n=1,
-#         stop=None,
-#         temperature=temperature,
-#     )
-#     response = gpt_response["choices"][0]["message"]["content"].strip()
-#     response = response
-#     return response
+    Guidelines:
+    - Shorten and keep the points crisp and strictly under 15 words.
+    - Get gacts from each list of article. not rely only on one or two source, for example, if 10 sources are present try to get facts from each sources. 
+    - Facts should be stastics based and have numbers in it. 
+    - The facts should be arranged in an ordered, numbered list.
+    - Each fact should include the source in the format '[source: [domain_name](URL)]'. Replace 'domain_name' with the actual domain name from the list of articles, and 'URL' with the complete URL from the article list. The source should be hyperlinked with the anchor text representing the source name, never create new list to show source, only hyperlink them in the anchor text at the end of each facts.
+    - The source link should be with nofollow tag. Please follow this strictly. example: <li>Home pregnancy tests are about 97-99% accurate if instructions are followed carefully. <a href="https://www.cdc.gov/nchs/fastats/births.htm" target="_blank" rel="noreferrer noopener nofollow">source: CDC</a></li>
+    '''
+    tokens = nltk.word_tokenize(prompt)
+    return len(tokens)
+    
 
 @st.cache_data(show_spinner=False)
-def generate_facts(list_of_article, topic, num_facts, model="gpt-3.5-turbo-16k", max_tokens=3000, temperature=0.2):
-    if len(list_of_article) > 12000:
-        # Split the list_of_article into three parts
-        part_length = len(list_of_article) // 6
+def generate_facts(list_of_article, topic, num_facts, model="gpt-3.5-turbo-16k", max_tokens=2000, temperature=0.2):
+
+
+# Create a separate copy of the list using slicing
+    # copy_list = list_of_article[:]
+
+    # word_count = 0
+    # for sublist in copy_list:
+    #     for string in sublist:
+    #         words = string.split()
+    #         word_count += len(words)
+
+    # st.write(word_count)
+    # st.write(list_of_article)
+    token_count = count_tokens(list_of_article, topic, num_facts)
+    
+    if token_count > 9000:
+        # st.write("inside first if")
+       
+        part_length = len(list_of_article) // 3
         list_of_article_part1 = list_of_article[:part_length]
-        list_of_article_part2 = list_of_article[part_length:2*part_length]
-        list_of_article_part3 = list_of_article[2*part_length:]
-        
-        # Generate facts for each part
-        response_part1 = generate_facts_helper(list_of_article_part1, topic, num_facts, model, max_tokens, temperature)
-        response_part2 = generate_facts_helper(list_of_article_part2, topic, num_facts, model, max_tokens, temperature)
-        response_part3 = generate_facts_helper(list_of_article_part3, topic, num_facts, model, max_tokens, temperature)
-        
-        # Combine the responses
-        response = response_part1 + response_part2 + response_part3
+
+        part_length = len(list_of_article) // 3
+        list_of_article_part1 = list_of_article[:part_length]
+
+
+        if count_tokens(list_of_article_part1, topic, num_facts) > 9000:
+            url = list_of_article_part1[0][0]  
+            article_text1 = "".join([element[1] for element in list_of_article_part1])  
+
+            # st.write("inside list part 1")
+            part_length1 = len(article_text1) // 3
+            part1 = [url, article_text1[:part_length1]]
+            part2 = [url, article_text1[part_length1: part_length1 * 2]]
+            part3 = [url, article_text1[part_length1 * 2:]]
+
+            
+            response_part1 = generate_facts_helper(part1, topic, num_facts)
+            response_part2 = generate_facts_helper(part2, topic, num_facts)
+            response_part3 = generate_facts_helper(part3, topic, num_facts)
+
+           
+            response1 = response_part1 + response_part2 + response_part3
+        else:
+            response1 = generate_facts_helper(list_of_article_part1, topic, num_facts)
+
+        # st.write(response1)
+
+        # Check if token count of list_of_article_part2 exceeds 13000
+        list_of_article_part2 = list_of_article[part_length: 2 * part_length]
+        if count_tokens(list_of_article_part2, topic, num_facts) > 9000:
+
+            # st.write("inside list part 2")
+            url = list_of_article_part2[0][0]  # Extract URL from the first element of list_of_article_part2
+            article_text2 = "".join([element[1] for element in list_of_article_part2])  # Concatenate article texts
+
+            # Split article_text2 into three parts
+            part_length2 = len(article_text2) // 3
+            part4 = [url, article_text2[:part_length2]]
+            part5 = [url, article_text2[part_length2: part_length2 * 2]]
+            part6 = [url, article_text2[part_length2 * 2:]]
+
+            # Generate facts for each part
+            response_part4 = generate_facts_helper(part4, topic, num_facts)
+            response_part5 = generate_facts_helper(part5, topic, num_facts)
+            response_part6 = generate_facts_helper(part6, topic, num_facts)
+
+            # Combine the responses
+            response2 = response_part4 + response_part5 + response_part6
+        else:
+            response2 = generate_facts_helper(list_of_article_part2, topic, num_facts)
+
+        # st.write(response2)
+
+        # Check if token count of list_of_article_part3 exceeds 13000
+        list_of_article_part3 = list_of_article[2 * part_length:]
+        if count_tokens(list_of_article_part3, topic, num_facts) > 9000:
+
+            # st.write("inside list part 3")
+            url = list_of_article_part3[0][0]  # Extract URL from the first element of list_of_article_part3
+            article_text3 = "".join([element[1] for element in list_of_article_part3])  # Concatenate article texts
+
+            # Split article_text3 into three parts
+            part_length3 = len(article_text3) // 3
+            part7 = [url, article_text3[:part_length3]]
+            part8 = [url, article_text3[part_length3: part_length3 * 2]]
+            part9 = [url, article_text3[part_length3 * 2:]]
+
+            # Generate facts for each part
+            response_part7 = generate_facts_helper(part7, topic, num_facts)
+            response_part8 = generate_facts_helper(part8, topic, num_facts)
+            response_part9 = generate_facts_helper(part9, topic, num_facts)
+
+            # Combine the responses
+            response3 = response_part7 + response_part8 + response_part9
+        else:
+            response3 = generate_facts_helper(list_of_article_part3, topic, num_facts)
+
+        # st.write(response3)
+
+        total = response1 + response2 + response3
+        st.write(total)
+        # st.write("final response")
+        # st.write(type(response))
+        # st.write(response)
+        # st.write("filtered reponse")
+        response = filter_response(total, topic, num_facts)
+
     else:
+        # st.write("inside else")
         response = generate_facts_helper(list_of_article, topic, num_facts, model, max_tokens, temperature)
     
     return response
 
 
-import re
-import openai
 
-import re
-import openai
+def filter_response(response_list, topic, num_facts, model="gpt-3.5-turbo-16k", max_tokens=1000, temperature=0.2):
+
+    prompt = f'''We have a list of facts that require filtering. Your task is to randomly extract {num_facts} facts from the given list that are more suitable for the topic: "{topic}". Please adhere to the following guidelines:
+
+    - Preserve the original wording of the facts; do not rewrite them. Simply select and filter {num_facts} facts from the list.
+    - While filtering, randomly choose facts that are relevant to the topic: "{topic}". Ensure that the facts come from different sources,rather than a single source, but it should not go against the given data if in given data multiple sources are not available then never add random sources by your own. You can identify the different sources by looking for the "source: [source name]" at the end of each fact.
+    - Retain any links present in the facts. If possible, add the "nofollow" attribute to the links.
+
+    Here is the fact list for reference: {response_list}.'''
+
+    gpt_response = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content":  prompt,
+            },
+            
+        ],
+        max_tokens=max_tokens,
+        n=1,
+        stop=None,
+        temperature=temperature,
+    )
+    response = gpt_response["choices"][0]["message"]["content"].strip()
+    response = response
+    return response
+
+
+
 @st.cache_data(show_spinner=False)
-def generate_facts_helper(list_of_article, topic, num_facts, model, max_tokens, temperature):
+def generate_facts_helper(list_of_article, topic, num_facts, model="gpt-3.5-turbo-16k", max_tokens=2000, temperature=0.2):
     prompt = f'''
     You are a specialized medical researcher or practitioner. A collection of articles titled '{list_of_article}' that delve into the subject matter of '{topic}' has been handed to you. Each article comes with the web page source from which the data has been procured. Your role involves conducting a detailed review and comprehension of these articles, discarding any superfluous information, and subsequently drawing out {num_facts} crisp facts to the provided topic. Each fact needs to be straightforward, and formatted under 15 words. These facts are to be organized in a numbered list with the accompanying source link for each fact, denoting the domain name of the source website.
 
@@ -264,7 +354,7 @@ def generate_facts_helper(list_of_article, topic, num_facts, model, max_tokens, 
     # filtered_facts = filtered_facts[:20]  # Select only the first 10 filtered facts
     # response = "\n".join(filtered_facts)  # Join the selected facts into a single string
     
-    st.write(num_facts)
+    # st.write(num_facts)
     return response
 
 def generate_facts_box(keyword, num_facts):
@@ -281,7 +371,7 @@ def generate_facts_box(keyword, num_facts):
             else:
                 article_text = scrape_article(url)
                 
-            filtered_url_df.at[index, "Article Text"] = article_text
+            filtered_url_df.at[index, "Article Text"] = re.sub('\s+', ' ', article_text).strip()
         except:
             filtered_url_df.at[index, "Article Text"] = ""
 
@@ -297,12 +387,13 @@ def generate_facts_box(keyword, num_facts):
 
     # st.write(article_text_list)
     # st.write(article_text_list)
-    facts = generate_facts(article_text_list, concated_keyword, num_facts)
     
-
-
     st.write(filtered_url_df)
+    token_count = count_tokens(article_text_list, concated_keyword, num_facts)
+    st.write(token_count)
 
+
+    facts = generate_facts(article_text_list, concated_keyword, num_facts)
     st.write(facts)
 
 def main():
